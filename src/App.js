c46e7410bb761415a77243146f03c4ce880b7720
Aliel Reyes <alielreyes@gmail.com>
2020-01-09 22:56:04 -0800

    Skate model now shows. Z-axis now works when scrolling. Working on setting different camera angles to choose from.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 //Components
 import P5Wrapper from 'react-p5-wrapper';
@@ -7,28 +9,66 @@ import sketch from './components/Sketch/sketch';
 //import SketchControls from './components/SketchControls/SketchControls';
 //import Slider from './components/Slider/Slider';
 
+//Bootstrap Components
+import ButtonGroup from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
+
 function App() {
 
   //TODO: Experiment creating a slider to contol zoom.
-  //Experiement in creating a button to reset the z-axis to normal.
-  //Set state using hooks.
+  //Zoom slider needs current camera coordinates. 
+
 
   const buttonIDs = {
     changeColorBtn: 'changeColorBtn',
-    resetCameraBtn: 'resetViewBtn'
+    resetCameraBtn: 'resetViewBtn',
+    changeDeckTextureBtn: 'changeDeckTextureBtn',
+    zoomSlider: "zoomSlider",
+    viewPanels: "viewPanels"
   }
+
+  const skatePositions = {
+    DYNAMIC: "dynamic",
+    FRONT: 'front',
+    SIDE: 'side',
+    TOP: 'top',
+    BOTTOM: 'bottom'
+}
+
+  //Camera Zoom Properties, TEMP
+  const zoomProps = {
+    min: 150,
+    max: 600,
+    default: 600
+  }
+
+  //State variables
   const [color, setColor] = useState(null);
   const [cameraCoord, setCameraCoord] = useState([0,0,350, 0,0,0, 0,1,0]);
   const [btnCaller, setBtnCaller] = useState(null);
+  const [deckTexture, setDeckTexture] = useState(require('./assets/starwars_example.jpeg'));
+  const [zoom, setZoom] = useState(450);
+  const [skatePosition, setSkatePosition] = useState(skatePositions.FRONT);
 
   const changeColorHandler = (event, color=[255, 0, 0]) => {
     setBtnCaller(event.target.id);
     setColor(color);
   }
-
   const resetCameraHandler = (event, coord=[0,0,350, 0,0,0, 0,1,0]) => {
     setBtnCaller(event.target.id);
     setCameraCoord(coord);
+  }
+  const changeDeckTextureHandler = (event, texture=require('./assets/volcom-truck-texture.jpg')) => {
+    setBtnCaller(event.target.id);
+    setDeckTexture(texture);
+  }
+  const zoomSliderHandler = (event) => {
+    setBtnCaller(event.target.id);
+    setZoom(event.target.value);
+  }
+  const changeSkatePos = (position) => {
+    setBtnCaller(buttonIDs.viewPanels);
+    setSkatePosition(position);
   }
 
   return (
@@ -40,6 +80,12 @@ function App() {
         cameraPos={cameraCoord}
         btnCaller={btnCaller}
         buttonIDs={buttonIDs}
+        deckTexture={deckTexture}
+        zoom={zoom}
+
+        //Positions
+        skatePosition={skatePosition}
+        skatePositions={skatePositions}
         />
 
         {/** On Change Prop trigger. */}
@@ -49,9 +95,20 @@ function App() {
         <button id={buttonIDs.resetCameraBtn} onClick={resetCameraHandler}>
           Reset Camera
         </button>
+        <button id={buttonIDs.changeDeckTextureBtn} onClick={changeDeckTextureHandler}>
+          Change Deck
+        </button>
+        <input id={buttonIDs.zoomSlider} type="range" min={zoomProps.min} max={zoomProps.max} default={zoomProps.default} onChange={zoomSliderHandler} />
+        
+        <div>
+          <Button onClick={()=>changeSkatePos(skatePositions.DYNAMIC)} variant="secondary">Dynamic</Button>
+          <Button onClick={()=>changeSkatePos(skatePositions.FRONT)} variant="secondary">Front</Button>
+          <Button onClick={()=>changeSkatePos(skatePositions.TOP)} variant="secondary">Top</Button>
+          <Button onClick={()=>changeSkatePos(skatePositions.SIDE)} variant="secondary">Side</Button>
+          <Button onClick={()=>changeSkatePos(skatePositions.BOTTOM)} variant="secondary">Bottom</Button>
+        </div>
+
       </div>
-
-
     </div>
   );
 }
--- src/components/Sketch/sketch.js
+++ src/components/Sketch/sketch.js
@@ -1,17 +1,77 @@
 export default function sketch (p) {
     let canvas;
-
-    //Prop variables
+    let texture;
+    let skateModel;
     let color;
+
+    let skatePos;
+
+    /**
+     * 1. Divide Skateboard into separate objects
+        * a. Decktop - tape texture
+        * b. Deckbottom - deck texture
+        * c. 4 Wheels - Wheel texture
+        * d. 2 Trucks - Truck texture
+     * 2. Design the control panels
+     * 3. Swap camera:
+     * 4. Design Initial Position
+     * 5. Change positions
+    * */
+
+    const setSkatePosition = (pos, skatePoses) => {
+        const modelPos = {
+            dynamic: {x:-60, y:-10, z:15},
+            front: {x:-90,y:0,z:0},
+            side: {x:-90,y:0,z:90},
+            top: {x:0,y:180,z:0},
+            bottom: {x:0,y:0,z:0}
+        };
+
+        switch(pos) {
+            case skatePoses.DYNAMIC:
+                skatePos = {...modelPos.dynamic};
+                break;
+            case skatePoses.FRONT:
+                skatePos = {...modelPos.front};
+                break;
+            case skatePoses.SIDE:
+                skatePos = {...modelPos.side};
+                break;
+            case skatePoses.TOP:
+                skatePos = {...modelPos.top};
+                break;
+            case skatePoses.BOTTOM:
+                skatePos = {...modelPos.bottom};
+                break;
+            default:
+                console.log('No Position Chosen');
+        }
+
+        return skatePos;
+    }
+
+    const toRadians = function (x) {
+        return x * Math.PI/180;
+    }
+
+    p.preload = function () {
+        skateModel = p.loadModel(require('./../../assets/Skateboard/skateboard.obj'), true);
+    }
 
     p.setup = function () {
         canvas = p.createCanvas(600, 400, p.WEBGL);
         color = [255, 255, 255]; //default
+        texture = p.loadImage(require('./../../assets/starwars_example.jpeg'));
     };
 
     p.myCustomRedrawAccordingToNewPropsHandler = function (newProps) {
+        
+        //Initial State 
+        setSkatePosition(newProps.skatePosition, newProps.skatePositions);
+
         //Redraw when a prop changes.
         if(canvas){
+
             switch(newProps.btnCaller){
                 case newProps.buttonIDs.changeColorBtn:
                     console.log('Color Change!');
@@ -21,22 +81,40 @@ export default function sketch (p) {
                     console.log('Reset Camera!');
                     p.camera(...newProps.cameraPos);
                     break;
+                case newProps.buttonIDs.changeDeckTextureBtn:
+                    console.log("Texture Change!");
+                    texture = p.loadImage(newProps.deckTexture);
+                    break;
+                case newProps.buttonIDs.zoomSlider:
+                    console.log("Zoom Change! " + newProps.zoom);
+                    p.camera(0,0,newProps.zoom, 0,0,0, 0,1,0);
+                    break;
+                case newProps.buttonIDs.viewPanels:
+                    console.log("Changed View " + newProps.skatePosition);
+                    setSkatePosition(newProps.skatePosition, newProps.skatePositions);
+                    break;
                 default:
                     console.log("Error, Default State Reached");
             }
+
         }
 
     };
 
     p.draw = function () {
 
-        p.orbitControl(1, 1);
+        p.orbitControl(3, 3, 0.03);
         p.background(100);
         p.normalMaterial();
-        p.noStroke();
+
+        //Initial Position
+        p.rotateX(toRadians(skatePos.x));
+        p.rotateY(toRadians(skatePos.y));
+        p.rotateZ(toRadians(skatePos.z));
+
         p.push();
-        p.fill(color);
-        p.box(100);
+        p.texture(texture);
+        p.model(skateModel);
         p.pop();
     };
 };
