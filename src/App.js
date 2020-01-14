bed34bcb35d0e4c90467de65b9691be9d139e37f
Aliel Reyes <alielreyes@gmail.com>
2020-01-13 21:35:25 -0800

    Header Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, {useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Header from './components/Header/Header';
+
 function App() {
 
   return (
     <div className="App">
-      <h1>TESTING APP</h1>
+      <Header title="Skate Builder" />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+//Custom style
+import classes from './Header.module.css';
+
+//Bootstrap
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Image from 'react-bootstrap/Image';
+
+function Header() {
+
+    const logo = require('./../../assets/Logos/site-logo.png');
+
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">
+                <Image src={logo} alt={'Logo'} className={classes.logo} fluid={true}/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="justify-content-end" style={{width: '100%'}}>
+                    <Nav.Item>
+                        <Nav.Link href="#myboards">MyBoards</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#cart">Cart</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default Header;
