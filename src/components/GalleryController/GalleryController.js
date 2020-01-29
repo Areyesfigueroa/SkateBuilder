786e57edb1086d9d2aa39e56ba278768d78b9479
Aliel Reyes <alielreyes@gmail.com>
2020-01-28 20:53:56 -0800

    Desktop reponsive layout changes.

--- src/components/GalleryController/GalleryController.js
+++ src/components/GalleryController/GalleryController.js
@@ -19,17 +19,17 @@ const GalleryController = (props) => {
     const responsive = {
       desktop: {
         breakpoint: { max: 3000, min: 1024 },
-        items: props.displayItemsLength,
+        items: 2,
         slidesToSlide: 1, // optional, default to 1.
       },
       tablet: {
         breakpoint: { max: 1024, min: 464 },
-        items: props.displayItemsLength,
+        items: 1,
         slidesToSlide: 1, // optional, default to 1.
       },
       mobile: {
         breakpoint: { max: 464, min: 0 },
-        items: props.displayItemsLength,
+        items: 1,
         slidesToSlide: 1, // optional, default to 1.
       },
     };
