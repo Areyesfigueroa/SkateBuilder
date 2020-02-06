7eae8862f10283fbffd9ebd6dfde7e3fe45c35a6
Aliel Reyes <alielreyes@gmail.com>
2020-02-06 09:50:41 -0800

    Initial Build

---
+++ .netlify/build/precache-manifest.182580b2f315fea9bb1523dfff5f0ca5.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f80c24af7fd63c114ecf7cc85c1e4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac13f79d0a90ccb60c59",
+    "url": "/static/css/2.73b44ea8.chunk.css"
+  },
+  {
+    "revision": "95eff5080fd52c815870",
+    "url": "/static/css/main.4dab24f4.chunk.css"
+  },
+  {
+    "revision": "ac13f79d0a90ccb60c59",
+    "url": "/static/js/2.8cce1179.chunk.js"
+  },
+  {
+    "revision": "d61afaddee5ef914fbf75c454cdf3763",
+    "url": "/static/js/2.8cce1179.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95eff5080fd52c815870",
+    "url": "/static/js/main.cddf3786.chunk.js"
+  },
+  {
+    "revision": "96fb144d4590a66f30a0",
+    "url": "/static/js/runtime-main.9e746526.js"
+  },
+  {
+    "revision": "50b377bfbbefa1b8112a119b1a59dc05",
+    "url": "/static/media/DECK_BOTTOM.50b377bf.obj"
+  },
+  {
+    "revision": "ad50ad117faa1ed95a81de92841db17c",
+    "url": "/static/media/DECK_MIDDLE.ad50ad11.obj"
+  },
+  {
+    "revision": "2c6fddd7615a483e6000550eee76570f",
+    "url": "/static/media/DECK_TOP.2c6fddd7.obj"
+  },
+  {
+    "revision": "87f0c11e5387c2541f63140ea61e44c4",
+    "url": "/static/media/TRUCK.87f0c11e.obj"
+  },
+  {
+    "revision": "7465cea52a881945be9d147e13ee52f3",
+    "url": "/static/media/WHEEL.7465cea5.obj"
+  },
+  {
+    "revision": "c0d613be9e78e002bb54985a0d881e5f",
+    "url": "/static/media/deck_bottom_001.c0d613be.jpg"
+  },
+  {
+    "revision": "f0ebd6d772eaa8221f7b9ab82f4e41a5",
+    "url": "/static/media/deck_bottom_002.f0ebd6d7.jpg"
+  },
+  {
+    "revision": "983aea151f64994219f9599a4f48a9fa",
+    "url": "/static/media/deck_bottom_003.983aea15.jpg"
+  },
+  {
+    "revision": "c22d44a9f5315a76ae1a4f1939121913",
+    "url": "/static/media/deck_middle_001.c22d44a9.jpg"
+  },
+  {
+    "revision": "c4f6368192eb7528af23b5b63b22b10c",
+    "url": "/static/media/deck_top_001.c4f63681.jpg"
+  },
+  {
+    "revision": "1d166109cf556db72201ed69a30c7640",
+    "url": "/static/media/deck_top_002.1d166109.jpg"
+  },
+  {
+    "revision": "438177a10408639bba029fb0956a1e95",
+    "url": "/static/media/deck_top_003.438177a1.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "6b140e1a58f609736a7ab85ad580409b",
+    "url": "/static/media/site-logo.6b140e1a.png"
+  },
+  {
+    "revision": "1ce7523cac5340c05fe142820ff8b297",
+    "url": "/static/media/textura.1ce7523c.png"
+  },
+  {
+    "revision": "112152845f2056af696bf39befd74913",
+    "url": "/static/media/trucks_001.11215284.jpg"
+  },
+  {
+    "revision": "fab456672927f3d2efe32eacb9ab2f81",
+    "url": "/static/media/trucks_002.fab45667.jpg"
+  },
+  {
+    "revision": "c152deb499c85b6c44828fddb2debf70",
+    "url": "/static/media/trucks_004.c152deb4.jpg"
+  },
+  {
+    "revision": "4041cb2e89b88429da88940413eb3a2e",
+    "url": "/static/media/uv-map.4041cb2e.jpg"
+  },
+  {
+    "revision": "51e1e390a7b0cc3deaf8526ebc5a4161",
+    "url": "/static/media/wheels_001.51e1e390.jpg"
+  },
+  {
+    "revision": "b6723f72406f569fedafcf1a72e96f71",
+    "url": "/static/media/wheels_002.b6723f72.jpg"
+  }
+]);
